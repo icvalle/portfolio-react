05bc1e85632ab25ce9efe8abc6c97a0a120e2b59
Iris Valle <iris.valle.design@gmail.com>
2021-08-01 13:25:55 -0400

    adding helpers

---
+++ src/utils/helpers.js
@@ -0,0 +1,12 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export function checkValue(input) {
+    const letters = /[a-zA-Z]+/g;
+    if (input.match(letters)) {
+      return true;
+    }
+    return false;
+}

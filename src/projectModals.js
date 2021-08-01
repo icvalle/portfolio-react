4ed3299dbdfb38e14931dd7b03e38a8b0ac3b73b
Iris Valle <iris.valle.design@gmail.com>
2021-08-01 13:28:24 -0400

    adding modals and project data

---
+++ src/projectModals.js
@@ -0,0 +1,67 @@
+const projectModals = [
+    {
+        content: {
+            title: "Movies Booking App",
+            id: "moviesBooking",
+            imgSrc: "assets/images/movies.png",
+            deployedLink: "https://limitless-earth-12683.herokuapp.com/api/customer/homepage",
+            repository: "https://github.com/reggiemace/project-2-team-2",
+        },
+    },
+    {
+        content: {
+            title: "Weather Dashboard",
+            id: "weatherDash",
+            imgSrc: "assets/images/weather_dashboard.jpg",
+            deployedLink: "https://icvalle.github.io/weather-dashboard/",
+            repository: "https://github.com/icvalle/weather-dashboard",
+        },
+    },
+    {
+        content: {
+            title: "Work Day Scheduler",
+            id: "workScheduler",
+            imgSrc: "assets/images/work_day_scheduler.jpg",
+            deployedLink: "https://icvalle.github.io/work-day-scheduler/",
+            repository: "https://github.com/icvalle/work-day-scheduler",
+        },
+    },
+    {
+        content: {
+            title: "Music Mashup",
+            id: "musicMash",
+            imgSrc: "assets/images/music_mashup.jpg",
+            deployedLink: "https://icvalle.github.io/music-mashup/",
+            repository: "https://github.com/icvalle/music-mashup",
+        },
+    },
+    {
+        content: {
+            title: "Budget Tracker",
+            id: "budgetTracker",
+            imgSrc: "assets/images/budgetTracker.jpg",
+            deployedLink: "https://damp-sands-25430.herokuapp.com/",
+            repository: "https://github.com/icvalle/progressive-budget",
+        },
+    },
+    {
+        content: {
+            title: "Note Taker",
+            id: "noteTaker",
+            imgSrc: "assets/images/noteTaker.jpg",
+            deployedLink: "https://evening-ravine-45003.herokuapp.com/",
+            repository: "https://github.com/icvalle/note-taker",
+        },
+    },
+    // {
+    //     content: {
+    //         title: "Sample",
+    //         id: "demo",
+    //         imgSrc: "assets/images/spotlight_work_sample.jpg",
+    //         deployedLink: "",
+    //         repository: "",
+    //     },
+    // },
+];
+
+export default projectModals;
---
+++ src/samples.js
@@ -0,0 +1,67 @@
+const samples = [
+    {
+        app: {
+            name: "Movies Booking App",
+        },
+        attribute: {
+            id: "spotlight",
+            target: "#moviesBooking",
+        },
+    },
+    {
+        app: {
+            name: "Weather Dashboard",
+        },
+        attribute: {
+            id: "weatherFig",
+            target: "#weatherDash",
+        },
+    },
+    {
+        app: {
+            name: "Work Day Scheduler",
+        },
+        attribute: {
+            id: "schedulerFig",
+            target: "#workScheduler",
+        },
+    },
+    {
+        app: {
+            name: "Music Mashup",
+        },
+        attribute: {
+            id: "musicFig",
+            target: "#musicMash",
+        },
+    },
+    {
+        app: {
+            name: "Budget Tracker",
+        },
+        attribute: {
+            id: "budgetFig",
+            target: "#budgetTracker",
+        },
+    },
+    {
+        app: {
+            name: "Note Taker",
+        },
+        attribute: {
+            id: "noteFig",
+            target: "#noteTaker",
+        },
+    },
+    // {
+    //     app: {
+    //         name: "Project",
+    //     },
+    //     attribute: {
+    //         id: "samples",
+    //         target: "#demo",
+    //     },
+    // },
+];
+
+export default samples;
